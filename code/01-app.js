06954158ddcc9eac22a43dc73562848ecaf9fe12
Rebecca Lain <35633997+RLain@users.noreply.github.com>
2022-06-03 12:29:25 +0200

    Update 01-app.js

--- code/01-app.js
+++ code/01-app.js
@@ -11,7 +11,7 @@
    method: 'POST',
    headers: {
      'X-RapidAPI-Host': 'faceanalysis.p.rapidapi.com',
-     'X-RapidAPI-Key': '8131367a1emsh2d72e84a887ca6cp12e591jsn6656c5ca1cda' //API_KEY 
+     'X-RapidAPI-Key': API_KEY 
    },
    body: data
  };
